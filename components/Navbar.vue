ef9f21781c9ddd856d93d9ca1e1db82499175a5a
Wahyu Sanjaya <wahyusnjy@gmail.comm>
2022-07-12 18:19:28 +0700

    First Commit

---
+++ components/Navbar.vue
@@ -0,0 +1,55 @@
+<template>
+<header class="flex items-center">
+            <div style="height: 54px" class="pr-5">
+              <img src="/logo.svg" alt="logo" class="h-full" />
+            </div>
+            <ul class="flex items-center">
+              <li>
+                <NuxtLink
+                  class="text-white hover:text-teal-500 text-lg px-4 py-3"
+                  to="/"
+                  >Home</NuxtLink
+                >
+              </li>
+              <li>
+                <a
+                  class="text-white hover:text-teal-500 text-lg px-4 py-3"
+                  to="#"
+                  >Project</a
+                >
+              </li>
+              <li>
+                <a
+                  class="text-white hover:text-teal-500 text-lg px-4 py-3"
+                  to="#"
+                  >Features</a
+                >
+              </li>
+              <li>
+                <a
+                  class="text-white hover:text-teal-500 text-lg px-4 py-3"
+                  to="#"
+                  >Success Stories</a
+                >
+              </li>
+            </ul>
+            <ul class="flex ml-auto items-center mt-2">
+              <li>
+                <NuxtLink
+                  to="/register"
+                  class="inline-block bg-transparent border-white border hover:bg-white hover:bg-opacity-25 text-white font-light w-40 text-center px-6 py-1 text-lg rounded-full mr-4"
+                >
+                  Sign Up
+                </NuxtLink>
+              </li>
+              <li>
+                <NuxtLink
+                  to="/login"
+                  class="inline-block bg-transparent border-white border hover:bg-white hover:bg-opacity-25 text-white font-light w-40 text-center px-6 py-1 text-lg rounded-full"
+                >
+                  My Account
+                </NuxtLink>
+              </li>
+            </ul>
+          </header>
+</template>
